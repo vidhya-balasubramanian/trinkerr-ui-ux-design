8883fc28acd5cae3a0eeca2161cf03d7e8dda3c3
vidhya <vidhya.inboxme@gmail.com>
2021-10-10 20:12:44 +0530

    ui alignment fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css';
+import '../styles/footer.css';
+import '../styles/header.css';
+import '../styles/main-content.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
